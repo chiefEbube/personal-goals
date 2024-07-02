91598d8f75cf8f742f8835a8b044ba1561991734
Ebube Anyanwu <ebubechi.anyanwu@yahoo.com>
2024-07-03 00:55:30 +0100

    updated updateTime function to display current day

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     function updateTime() {
+        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         const now = new Date();
+
         const timeUTC = now.toUTCString().split(' ')[4];
-        const dayUTC = now.toUTCString().split(',')[0];
+        const dayIndex = now.getUTCDay();
+        const dayUTC = daysOfWeek[dayIndex];
 
         document.getElementById('currentTimeUTC').textContent = timeUTC;
         document.getElementById('currentDay').textContent = dayUTC;
